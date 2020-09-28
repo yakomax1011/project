8ce96b4c504ecbe5e92012b3cd5360096de86483
Yakomax <yakomax@system-pro.net>
2020-09-29 00:02:44 +0300

    29.09.2020

--- lesson_2_tasks_copy/test 2.py
+++
@@ -1,9 +0,0 @@
-seasons = {'winter': (1, 2, 12),
-           'spring': (3, 4, 5),
-           'summer': (6, 7, 8),
-           'autumn': (9, 10, 11)}
-
-month = int(input('Введите месяц вашего рождения: '))
-for key in seasons.keys():
-    if month in seasons[key]:
-        print(key)

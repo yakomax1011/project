7ddd6a06edadaec5db89275c693e1085afe496c3
Yakomax <yakomax@system-pro.net>
2020-09-29 09:43:48 +0300

    update

--- lesson_2_tasks_test/task3.py
+++ lesson_2_tasks_test/task3.py
@@ -3,22 +3,22 @@
 #Напишите решения через list и через dict.
 
 
-winter = [12, '01', 1, '02', 2, 'декабрь', 'январь', 'февраль']
-spring = ['03', 3, '04', 4, '05', 5, 'март', 'апрель', 'май']
-summer = ['06', 6, '07', 7, '08', 8, 'июнь', 'июль', 'август']
-autumn = ['09', 9, 10, 11, 'сентябрь', 'октябрь', 'ноябрь']
+winter = [12, 1, 2]
+spring = [3, 4, 5]
+summer = [6, 7, 8]
+autumn = [9, 10, 11]
 
-month = (input('Введите месяц вашего рождения: '))
+month = int(input('Введите месяц вашего рождения: '))
 
-for i in range(len(winter)), range(len(spring)), range(len(summer)), range(len(autumn)):
-    if month in winter:
-        print('Winter')
-    if month in spring:
-        print('Spring')
-    if month in summer:
-        print('Summer')
-    if month in autumn:
-        print('Autumn')
+#for i in range(len(winter)), range(len(spring)), range(len(summer)), range(len(autumn)):
+if month in winter:
+    print('Winter')
+if month in spring:
+    print('Spring')
+if month in summer:
+    print('Summer')
+if month in autumn:
+    print('Autumn')
 
 
 
